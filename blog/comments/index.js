af7174705d694c9b26e272fc18c92066a6c09e40
Martin Alexander Kretzschmar <alexander.martin64@gmail.com>
2021-04-27 17:18:03 -0300

    Added events

--- blog/comments/index.js
+++ blog/comments/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser')
 const { randomBytes } = require('crypto');
 const cors = require('cors');
+const axios = require('axios');
 
 const app = express();
 app.use(bodyParser.json());
@@ -21,9 +22,24 @@ app.post('/posts/:id/comments', (req, res) => {
 
     comments.push({ id: commentId, content });
 
+    await axios.post('http://localhost:4005/events', {
+        type: 'CommentCreated',
+        data: {
+            id: commentId,
+            content,
+            postId: req.params.id
+        }
+    });
+
     commentsBtPostId[req.params.id] = comments;
 
     res.send(201).send(comments);
+});
+
+app.post('/events', (req, res) => {
+    console.log('Received Event', req.body.type);
+
+    res.send({});
 });
 
 app.listen(4001, () => {
