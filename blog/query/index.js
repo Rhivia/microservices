6b3f29b74256f6ed60455f82f0273d6865c83b30
Martin Alexander Kretzschmar <alexander.martin64@gmail.com>
2021-04-28 09:55:00 -0300

    Updated comments list

--- blog/query/index.js
+++ blog/query/index.js
@@ -6,12 +6,31 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
-app.get('/posts', (req, res) => {
+const posts = {};
 
+app.get('/posts', (req, res) => {
+    res.send(posts);
 });
 
 app.get('/posts', (req, res) => {
-    
+    const { type, data } = req.data;
+
+    switch (type) {
+        case 'PostCreated':
+            const { id, title } = data;
+            posts[id] = { id, title, comments: [] };
+            break;
+        case 'CommentCreated':
+            const { id, content, postId, status } = data;
+
+            const post = posts[postId];
+            post.comments.push({ id, content, status });
+            break;
+        default:
+            break;
+    }
+
+    res.send({});
 });
 
 app.listen(4002, () => {
