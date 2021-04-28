69006da7b8dbd22b17bb5aae8c7f611375620f5e
Martin Alexander Kretzschmar <alexander.martin64@gmail.com>
2021-04-28 10:13:30 -0300

    Comment status condition

--- blog/client/src/CommentList.js
+++ blog/client/src/CommentList.js
@@ -1,8 +1,15 @@
 import React from 'react';
 
 export default ({ comments }) => {
+
     const renderedComments = comments.map(comment => {
-        return <li>{comment.content}</li>
+        let content;
+
+        if (comment.status === 'approved') content = comment.content;
+        if (comment.status === 'pending') content = 'Awaiting moderation';
+        if (comment.status === 'rejected') content = 'Comment Rejected';
+
+        return <li key={comment.id}>{content}</li>
     })
 
     return <ul>{renderedComments}</ul>;
